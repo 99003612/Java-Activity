6c4693991718bece37c88c67dc5dafe728e23ca2
Abhishek Vithal <40138618+vithalabhishek@users.noreply.github.com>
2021-02-27 21:39:32 +0530

    Update TestCustom.java

--- Day 4 Activities (PS 99003612)/Exception Handling/3/TestCustom.java
+++ Day 4 Activities (PS 99003612)/Exception Handling/3/TestCustom.java
@@ -7,26 +7,33 @@ public class TestCustom {
 	public static void main(String[] args) throws CustomException1 {
 		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Player details\nEnter Player Name");
-		playerName=sc.nextLine();
 		
-		System.out.println("Enter Player Age");
-		playerAge=sc.nextInt();
+		try
+		{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Player details");
+			System.out.println("Enter Player Name");
+			playerName=sc.nextLine();
 		
-		if(playerAge<19){
-			throw new CustomException1();
+			System.out.println("Enter Player Age");
+			playerAge=sc.nextInt();
+		
+			if(playerAge<19){
+				throw new CustomException1("InvalidAgeRangeException");
 			
+			}
+			else{
+				System.out.println("Player Registered");
+				System.out.println("Player Name: "+playerName);
+				System.out.println("Player Age: "+playerAge);
+			}
+		
 		}
-		else{
-			System.out.println("Player registered");
-			System.out.println("Player Name "+playerName);
-			System.out.println("Player Age "+playerAge);
-			
+		
+		catch(CustomException1 ce)
+		{
+			System.out.println(ce);
 		}
-		
-		
-		
-	}
 
+	}
 }
