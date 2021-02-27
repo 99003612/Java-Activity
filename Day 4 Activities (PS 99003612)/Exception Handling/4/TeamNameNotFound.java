96561d99a2baab583bf4ab9bd03248c3f1162579
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-27 17:50:04 +0530

    Create TeamNameNotFound.java

---
+++ Day 4 Activities (PS 99003612)/Exception Handling/4/TeamNameNotFound.java
@@ -0,0 +1,9 @@
+public class TeamNameNotFound extends Exception {
+	public TeamNameNotFound() {
+		
+		System.out.println("Entered team is not a part of IPL Season 13");
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
