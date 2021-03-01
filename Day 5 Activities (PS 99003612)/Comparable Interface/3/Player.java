23ffc215be9ee3d179055930c24a2d3ee1fcd894
Abhishek Vithal <40138618+vithalabhishek@users.noreply.github.com>
2021-03-02 00:42:11 +0530

    Create Player.java

---
+++ Day 5 Activities (PS 99003612)/Comparable Interface/3/Player.java
@@ -0,0 +1,27 @@
+
+public class Player implements Comparable<Player>
+{
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Player(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public int compareTo(Player o) {
+		
+		return 0;
+	}
+	
+	
+
+}
