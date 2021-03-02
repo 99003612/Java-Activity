b8fc4cdc1d0b9bf9e856698448accac27a584bef
Abhishek Vithal <40138618+vithalabhishek@users.noreply.github.com>
2021-03-03 00:07:10 +0530

    Update HashMapPlayerMain.java

--- Day 6 Activities (PS 99003612)/Hashmap/2/HashMapPlayerMain.java
+++ Day 6 Activities (PS 99003612)/Hashmap/2/HashMapPlayerMain.java
@@ -7,73 +7,113 @@ public class HashMapPlayerMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ArrayList<Wicket> w = new ArrayList<Wicket>();
+		ArrayList<Playerone> w = new ArrayList<Playerone>();
 		Scanner sc = new Scanner(System.in);
-		HashMap<Bowler,Wicket> hm = new HashMap<Bowler,Wicket>();
+		HashMap<Playerone> hm = new HashMap<Playerone	>();
 		
 		boolean b = true;
 		
+		boolean c = true;
+		String result = Boolean.toString(c);
+		
+		
+		boolean d = true;
+		String result1 =Boolean.toString(d);
+		
+		
 		
 	
-			System.out.println("Enter the player name ");
+	
+			System.out.println("Enter the player name: ");
 			String bname = sc.nextLine();
+			
+			System.out.println("Enter the wicket count: ");
+			int wcount = sc.nextInt();
 			
 		//HashMap<Bowler,Wicket> hm = new HashMap<Bowler,Wicket>();
 			
-			Bowler b1 = new Bowler(bname);
+			Playerone b1 = new Playerone(bname,wcount);
 		
 			hm.put(b1,null);
 		
 			System.out.println("Enter wickets - seperated by | symbol. ");
-			hm.put(b1, new Wicket(sc.nextLine(), b1));
+			hm.put(b1, new Playerone(sc.nextLine(), b1));
 		
-			System.out.println("Do you want to add another player(YES/NO");
-			boolean c = sc.nextBoolean();
+			System.out.println("Do you want to add another player(YES/NO)");
+			result = sc.next();
+			if(result.equalsIgnoreCase("yes"))
+			 {
+				 c=true;
+			 }
+			 else
+			 {
+				 c=false;
+			 }
+			
+			 while(c==true)
+			 {
+				
+				 System.out.println("Enter the player name ");
+				 bname = sc.nextLine();
+				 sc.nextLine();
 		
+	//HashMap<Bowler,Wicket> hm = new HashMap<Bowler,Wicket>();
 		
+				 b1 = new Bowler1(bname);
+	
+				 hm.put(b1,null);
+	
+				 System.out.println("Enter wickets - seperated by | symbol. ");
+				 hm.put(b1, new Wicket1(sc.nextLine(), b1));
+	
+				 System.out.println("Do you want to add another player(YES/NO)");
+				 result = sc.nextLine();
+				 if(result.equalsIgnoreCase("yes"))
+				 {
+					 c=true;
+				 }
+				 else
+				 {
+					 c=false;
+				 }
+						
+			}
+			 
 			
 		
 		
-		Wicket w1 = hm.get(b1);
+		Wicket1 w1 = hm.get(b1);
 		
 		String bow=b1.getName(); 
-		
+//		System.out.println(bow);
 		String w2= w1.getPlayerName();
 		
+//		System.out.println(w2);
 		String [] wrr = w2.split("\\|");
-		System.out.println(wrr[0]);
-		System.out.println(b1.toString());
+//		System.out.println(wrr[0]);
+//		System.out.println(b1.toString());
+		
 		System.out.println("Enter the player name to search ");
 		
 		String ser = sc.next();
-		//System.out.println(wrr[0]);
-		int count = 0 ;
-		int a = 0;
-	if(ser.equals(bow))
-			{
-	
-			
+			//System.out.println(wrr[0]);
+//		 for(int i=0;i<wrr.length;i++){
+//			 System.out.println(wrr[i]);
+//	            }    
+//		int a =0;
+		if(ser.equals(bow))
+		{
 			for(String e:wrr)
 			{
+				int a = wrr.length;
+				System.out.println(a);	
 				
-				a = wrr.length;
+			}
+		}
+			else
+			{
+				System.out.println("No Player found with the name "+ ser);
 			
 			}
-		
-			System.out.println(a);
-		//for (int i =0; i<wrr.length;i++){
-			
-			
-			//System.out.println(wrr[i]);
-			
-		}
-//		else
-//		{
-//			System.out.println(w2);
-//		}
-//		}
-//		
-//		
-}	
+	}
 }
-		
